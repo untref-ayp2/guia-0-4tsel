e41af8607ddb93b8665002fa043a4bbb328b66fb
Axel Novo <novo31297@estudiantes.untref.edu.ar>
2023-03-30 22:46:27 -0300

    Refactorizados los ciclos con range para el manejo de arrays

--- arreglos/main.go
+++ arreglos/main.go
@@ -2,25 +2,145 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 
-	arreglo:=[]int{1, 15, 8, 16, 66, 18}
+	arreglo := []int{7, 5, 9, 1, 6, 4, 8, 3, 1}
+	arreglo2 := []int{2, 1, 6, 4, 5, 0, 3, 9}
 
 	fmt.Println(suma(arreglo))
+
+	fmt.Println(union(arreglo, arreglo2))
+
+	fmt.Println(interseccion(arreglo, arreglo2))
+
 }
 
-/* 
+/*
 Escribir una función que reciba un arreglo de enteros como parámetros y devuelva la suma de todos sus elementos
 */
-func suma(arreglo []int) int{
+func suma(arreglo []int) int {
 
 	resultado := 0
 
-	for i := 0; i < len(arreglo); i++ {
-		
-		resultado += arreglo[i]
+	for _, v := range arreglo {
+
+		resultado += v
 	}
+
+	/* for i := 0; i < len(arreglo); i++ {
+
+		resultado += arreglo[i]
+	} */
 
 	return resultado
 }
 
+/*
+Escribir una función que reciba dos arreglos A y B, de N y M elementos respectivamente
+que representan conjuntos de enteros y devuelva una arreglo con la unión y otro con la intersección de A y B.
+*/
+func union(a, b []int) []int {
+
+	unido := a[:]
+
+	unido = append(unido, b...)
+
+	unido = eliminar_repetidos(unido)
+
+	return unido
+}
+
+func interseccion(a, b []int) []int {
+
+	intersec := []int{}
+
+	for _, v := range a {
+
+		aux, encontrado := 0, false
+
+		for !encontrado && aux < len(b) {
+
+			if v == b[aux] {
+
+				encontrado = true
+			}
+
+			aux++
+		}
+
+		if encontrado {
+
+			intersec = append(intersec, v)
+		}
+	}
+
+	/* for i := 0; i < len(a); i++ {
+
+		aux := 0
+		encontrado := false
+
+		for !encontrado && aux < len(b) {
+
+			if a[i] == b[aux] {
+
+				encontrado = true
+			}
+			aux++
+		}
+
+		if encontrado {
+
+			intersec = append(intersec, a[i])
+		}
+	} */
+
+	intersec = eliminar_repetidos(intersec)
+
+	return intersec
+}
+
+func eliminar_repetidos(a []int) []int {
+
+	var filtrado []int
+
+	filtrado = append(filtrado, a[0])
+
+	for _, v := range a {
+
+		encontrado, aux := false, 0
+
+		for !encontrado && aux < len(filtrado) {
+
+			if filtrado[aux] == v {
+
+				encontrado = true
+			}
+
+			aux++
+		}
+
+		if !encontrado {
+
+			filtrado = append(filtrado, v)
+		}
+	}
+
+	/* for i := 1; i < len(a); i++ {
+
+		encontrado := false
+		aux := 0
+		for !encontrado && aux < len(filtrado) {
+
+			if filtrado[aux] == a[i] {
+
+				encontrado = true
+			}
+			aux++
+		}
+		if !encontrado {
+			filtrado = append(filtrado, a[i])
+		}
+	} */
+
+	return filtrado
+}
